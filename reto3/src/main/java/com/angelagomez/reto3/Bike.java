385d829f7ebf3d7ebfad026010054dd6dd852e68
Ángela Gómez <139491058+MissGomezzz@users.noreply.github.com>
2026-02-15 14:03:55 -0500

    Delete reto3 directory

--- reto3/src/main/java/com/angelagomez/reto3/Bike.java
+++
@@ -1,9 +0,0 @@
-package com.angelagomez.reto3;
-
-
-public class Bike extends AbstractVehicle {
-
-    public Bike (CategoryStrategy category) {
-                super(1000000, 200, category, VehicleType.LAND, "Standard bike equipment");
-    }
-}
